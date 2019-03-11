4a1768d4df925744920f96c53289864ec7f0db9a
Yashashvi Dave <yashashvidave@gmail.com>
2019-03-11 19:10:19 +0530

    Step 3: Add toolbar button with js logic.

--- activities/Pawn.activity/js/activity.js
+++ activities/Pawn.activity/js/activity.js
@@ -1,4 +1,4 @@
-define(["sugar-web/activity/activity", "sugar-web/env"], function (activity, env) {
+define(["sugar-web/activity/activity", "sugar-web/env", "sugar-web/graphics/icon"], function (activity, env, icon) {
 
 	// Manipulate the DOM only when it is ready.
 	requirejs(['domReady!'], function (doc) {
@@ -6,11 +6,27 @@ define(["sugar-web/activity/activity", "sugar-web/env"], function (activity, env
 		// Initialize the activity.
 		activity.setup();
 
+		// Variables
+		var currentEnv;
+
 		// Get current user's name from `env` library
 		env.getEnvironment(function(err, environment) {
-						   document.getElementById("user").innerHTML = "<h1>"+"Hello"+" "+environment.user.name+
+
+			currentEnv = environment;
+			document.getElementById("user").innerHTML = "<h1>"+"Hello"+" "+environment.user.name+
 						   " Welcome to Pawn Activity!</h1>";
 		});
+
+		// Handle event: click on button add
+		document.getElementById("add-button").addEventListener('click', function (event) {
+			var pawn = document.createElement("div");
+			pawn.className = "pawn";
+
+			document.getElementById("pawns").appendChild(pawn);
+			icon.colorize(pawn, currentEnv.user.colorvalue);
+			document.getElementById("user").innerHTML = "<h1>"+currentEnv.user.name+" played !</h1>";
+		});
+
 	});
 
 });
